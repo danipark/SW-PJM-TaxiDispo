7fad6c1ff0d1fb3420b16770c3f8c797c84af291
LucasZirlewagen <45358223+LucasZirlewagen@users.noreply.github.com>
2020-12-30 13:20:53 +0100

    TAXI-35

--- src/app/Login_new/Registrierung/registrierungController.ts
+++ src/app/Login_new/Registrierung/registrierungController.ts
@@ -20,6 +20,11 @@ export class RegistrierungPage implements OnInit {
   constructor(private formBuilder: FormBuilder, private authService: AuthService) { }
  
   ngOnInit() {
+    this.formForRegister();
+    this.geocoderForAddress();
+  }
+
+  formForRegister() {
     this.registerForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
@@ -28,55 +33,50 @@ export class RegistrierungPage implements OnInit {
       address: ['', [Validators.required, Validators.minLength(5)]],
       points: [0]
     });
+  }
 
-    //Geocoder
+  geocoderForAddress() {
     this.geocoder = new MapboxGeocoder({
       accessToken: environment.mapboxKey,
       countries: 'de',
       placeholder: 'Addresse',
       mapboxgl: Mapboxgl
-      });
-      this.geocoder.addTo('#geocoder');
-    
+    });
+    this.geocoder.addTo('#geocoder');
   }
- 
-  radioGroupChange(event){
+
+  radioGroupChange(event) {
     this.selectedRadioGroup = event.detail.value;
-    console.log(this.selectedRadioGroup)
-    if (this.selectedRadioGroup == "Geschäftskunde"){
+    if (this.selectedRadioGroup == "Geschäftskunde") {
+      this.selectBusinessCustomer();
+    } else {
+      this.selectPrivateCustomer();
+    }
+  }
+
+  selectBusinessCustomer() {
       this.registerForm.addControl('role', new FormControl('', Validators.required));
       this.registerForm.controls['role'].setValue('businessCustomer')
       this.registerForm.addControl('companyName', new FormControl('', Validators.required));
-      
-    
-    } else {
-      this.registerForm.addControl('role', new FormControl('', Validators.required));
-      this.registerForm.controls['role'].setValue('customer')
-    }
-      
+  }
 
-   
-
-
-
+  selectPrivateCustomer() {
+    this.registerForm.addControl('role', new FormControl('', Validators.required));
+    this.registerForm.controls['role'].setValue('customer')
   }
 
   onSubmit() {
-
     //Addresse der Form hinzufügen
     var geocoderJSON = this.geocoder.lastSelected;
     let geocoderObject = JSON.parse(geocoderJSON);
     var address = geocoderObject.place_name;
 
-    console.log(address);
     this.registerForm.addControl('address', new FormControl('', Validators.required));
     this.registerForm.controls['address'].setValue(address)
 
     this.authService.register(this.registerForm.value).subscribe(res => {
       // Call Login um automatisch User einzuloggen
       this.authService.login(this.registerForm.value).subscribe();
-    });  
+    });
   }
- 
-
 }
